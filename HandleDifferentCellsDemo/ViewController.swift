5a2ffe2faf60d67e2a624270fb0fea70cfc21c14
bokhary <elsayed.husseinn15@gmail.com>
2020-08-21 16:13:33 +0300

    Initial Commit

---
+++ HandleDifferentCellsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HandleDifferentCellsDemo
+//
+//  Created by Elsayed Hussein on 8/21/20.
+//  Copyright © 2020 Elsayed Hussein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
